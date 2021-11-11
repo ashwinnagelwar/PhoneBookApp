95a6b0b72c57540e5a9afbae7897c5cc3f0647bd
Ashwin <ashwinnagelwar@gmail.com>
2021-11-11 17:48:32 +0530

    PHN-1:Controller method created

--- src/main/java/com/bikkadIt/phonebook/controller/ContactInfoController.java
+++ src/main/java/com/bikkadIt/phonebook/controller/ContactInfoController.java
@@ -2,7 +2,16 @@ package com.bikkadIt.phonebook.controller;
 
 import org.springframework.stereotype.Controller;
 
+import com.bikkadIt.phonebook.entity.Contact;
+
 @Controller
 public class ContactInfoController {
 
+	
+	
+	public String saveContact(Contact contact)
+	{
+		String s="Herllo";
+		return s;
+	}
 }
